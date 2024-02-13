3a02cb27ba6626c31e4e8f882489466a149cd238
As You Like it <157910680+AYLIT-crew@users.noreply.github.com>
2024-02-13 07:54:34 +0100

    update ark0.0.7(stable).py

--- ark0.0.7(stable).py
+++ ark0.0.7(stable).py
@@ -29,7 +29,7 @@ def arkmenu0():
         os.system('clear')
         os.system('python lib/Nexus.py')
     elif user_choice == '2':
-        print(cl('Requires nmap module installed to work', ''))
+        print(cl('Requires nmap module installed to work', 'red'))
         os.system('clear')
         os.system('python lib/net_scan.py')
     elif user_choice == '3':
