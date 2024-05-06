a36793f7e19e07bef81bf5ff2d7544314671a07c
As You Like it <157910680+AYLIT-crew@users.noreply.github.com>
2024-05-06 12:59:53 +0530

    Create whois.py

---
+++ lib/theEYE/whois.py
@@ -0,0 +1,20 @@
+import subprocess
+from termcolor import colored as cl
+from pyfiglet import figlet_format as ff
+
+subprocess.call('clear', shell=True)
+
+print(cl('='*31, 'red'))
+print(cl(ff('WhoIs'), 'red'))
+print(cl('='*31, 'red'))
+
+domain = input(cl('Enter domain:', 'yellow'))
+cmd = f'whois -H {domain}'
+data = subprocess.check_output(cmd.split()).decode()
+print(cl(data, 'blue'))
+
+inp = input(cl('Press enter to continue', 'red'))
+if inp == '':
+    subprocess.call('clear', shell=True)
+else:
+    subprocess.call('clear', shell=True)
