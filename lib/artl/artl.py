792452ff639fd04ae7a7e3ce359c236fdb5575e7
As You Like it. <157910680+AYLIT-crew@users.noreply.github.com>
2024-08-05 15:20:30 +0200

    update artl.py

--- lib/artl/artl.py
+++ lib/artl/artl.py
@@ -10,65 +10,12 @@ class Installer:
     def __init__(self):
         pass
     
-    # Print the list of available tools
-    def print_tool_list(self):
-        with open('tools_names.txt', 'r') as file:
-            tools = file.read()
-        print(colored('List of available tools:\n', 'cyan'))
-        print(tools)
-
-
-    # Install a tool from a given Git repository URL
-    def install_tool(self, repo_url):
-        try:
-            print(colored('Tool is being installed, please hold on...\n', 'red'))
-            subprocess.run(['git', 'clone', repo_url], check=True)
-            print(colored('Tool installed, enjoy.', 'red'))
-        except subprocess.CalledProcessError as e:
-            print(colored(f'Failed to install tool: {e}', 'red'))
-        sleep(1)
-        self.menu()
-
-    # Handle user choice for tool installation
-    #def handle_tool_choice(self, choice):
-#        tool_repos = {
-#            '1': 'https://github.com/aircrack-ng/aircrack-ng',
-#            '2': 'https://github.com/beefproject/beef',
-#            # Add more tools here as needed
-#        }
-#        repo_url = tool_repos.get(choice)
-#        if repo_url:
-#            self.install_tool(repo_url)
-#        else:
-#            print(colored('Invalid choice or not implemented yet.', 'red'))
-#            sleep(1)
-#            self.menu()
-
-    # Display the main menu and handle user input
-#    def main_menu(self):
-#            self.print_tool_list()
-#            usrchoice2 = input(colored('\nEnter the number of tool you want to install:\n', 'cyan'))
-#            self.handle_tool_choice(usrchoice2)
+    def instalando(self):
+        while True:
+            os.system('clear')
+           
     
-
-    # Display the welcome screen
-#   def welcome(self):
-#       os.system('clear')
-#       print(colored('-'*58, 'red'))
-#        print(colored(figlet_format('G U A R D I A') + '\n\t-An AYLIT Production\n\t-v1.0 beta', 'cyan'))
-#        print(colored('-'*58, 'red'))
-#        self.main_menu()
-# --- decided it won't be useful since I'll rewrite it a bit but I'll still keep it for future needs if something is needed ---
-
-    def exit_program(self):
-        print(colored('Exiting program', 'red'))
-        exit()
-
-    def invalid_selection(self):
-        print(colored('Invalid selection', 'red'))
-        sleep(2)
-        self.menu()
-
+    
     def menu(self):
         while True:
             os.system('clear')
