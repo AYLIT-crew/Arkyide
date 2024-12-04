4a03555986f52c7f0d67963ddcfabe708ed6a39f
As You Like it. <157910680+AYLIT-crew@users.noreply.github.com>
2024-12-04 14:47:46 +0100

    update artl.py

--- lib/artl/artl.py
+++ lib/artl/artl.py
@@ -1,7 +1,8 @@
-import pyfiglet
+import pyfiglet as pf
 import termcolor
 from termcolor import colored as cl
 import os
 
 def main():
     print
+#idfk whqt to write cuz i forgot python. rework soon i guess after i revise.
